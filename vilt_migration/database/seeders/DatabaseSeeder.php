44135f87b1438d45bc832aa5c01113430d272515
Brian Christopher L. Bulawan <brianbulawan5@gmail.com>
2026-03-29 08:31:19 +0800

    Add authentication using breeze

--- vilt_migration/database/seeders/DatabaseSeeder.php
+++ vilt_migration/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Add default seeding here
+        User::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin User',
+                'password' => Hash::make('password'),
+            ]
+        );
     }
 }
